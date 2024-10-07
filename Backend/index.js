010ae2a4139f5ef494babbe38e23157406d69c79
ABDULLATIFYOUSUF <latifyousuf513@gmail.com>
2024-10-07 06:24:23 -0700

    domain name changed

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const _dirname = path.resolve();
 
 const app = express()
 const corsOptions ={
-  origin: "https://boodstoreapp-production.up.railway.app",
+  origin: "https://latifbookstore.up.railway.app",
   Credentials:true  
 }
 app.use(cors(corsOptions))
